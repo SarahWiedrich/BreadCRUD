4b18ec8577a794276e7030e38df30bc189ea8a4b
SarahWiedrich <sknapcik@gmail.com>
2022-05-16 18:22:30 -0500

    all files created

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+const breadRoutes = require('./controllers/breads_controller')
+
+const app = express()
+
+//middleware
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+app.use('/breads', breadRoutes)
+
+//home page//
+app.get('/', (req,res) => {
+    res.send('Welcome to BREAD')
+})
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
