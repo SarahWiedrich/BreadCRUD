a3150b7547d1a4d8c3ecb58e2af6ce4df0eb7950
SarahWiedrich <sknapcik@gmail.com>
2022-05-17 21:15:21 -0500

    created add button and function - works well

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -9,9 +9,33 @@ router.get('/', (req, res) => {
     })
 })
 
+
+router.get('/new', (req,res) => {
+    res.render('new')
+})
+
+// get bread by index
 router.get('/:arrayIndex', (req, res) => {
     const { arrayIndex } = req.params
-    res.send(Bread[arrayIndex])
+    res.render('show', {
+        bread: Bread[arrayIndex]
+    })
+})
+
+//Create - post request
+router.post('/', (req, res) => {
+    if (!req.body.image) {
+        req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+    }
+    //make new bread and push into existing bread array
+    if(req.body.hasGluten === 'on'){
+        req.body.hasGluten = true
+    }else {
+        req.body.hasGluten = false
+    }
+
+    Bread.push(req.body)
+    res.redirect('/breads')
 })
 
 module.exports = router
